77ea47bfa04b40e8b6a87a40503620a6ae9e5778
Wilted98 <vasy.kr7@gmail.com>
2022-11-01 12:55:02 +0200

    Check again.

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const main = async () => {
   myDataSource
     .initialize()
     .then(() => {
-      console.log("Data Source has been initialized!");
+      console.log("Data Source has been initialized!!");
     })
     .catch((err) => {
       console.error("Error during Data Source initialization", err);
